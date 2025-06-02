613d5cd2a3cb47b451bbdb78674c2655474652eb
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-02 16:57:39 +0300

    DB table

--- QuerySort/urls.py
+++ QuerySort/urls.py
@@ -1,1 +1,12 @@
-urlpatterns = []
+from django.urls import path, include
+
+from QuerySort import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', include([
+        path('lector/', views.lector, name='add-lector'),
+        path('data/', views.add_data, name='add-data'),
+    ]
+    ))
+]
