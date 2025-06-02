b96f85c10c728a0dbc931480075fe61c71997778
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-02 16:43:52 +0300

    Styling changes

--- QuerySort/models.py
+++ QuerySort/models.py
@@ -4,12 +4,21 @@ from django.db import models
 # Create your models here.
 
 class Event(models.Model):
-    # The RegEx allows Latin and Cyrillic letters, digits, spaces, special characters and more
+    # The RegEx  '^[A-Za-zА-Яа-яЁё0-9 _\-\'&]+$' allows Latin and Cyrillic letters, digits, spaces, special characters and more
+
     name = models.CharField(max_length=255,
                             null=False,
                             blank=False,
                             validators=[
                                 MinLengthValidator(6, message='The event name must be at least 6 charracers long.'),
+                                RegexValidator('^[A-Za-zА-Яа-яЁё0-9 _\-\'&]+$')
+                            ])
+
+    town = models.CharField(max_length=255,
+                            null=False,
+                            blank=False,
+                            validators=[
+                                MinLengthValidator(6, message='The town name must be at least 6 charracers long.'),
                                 RegexValidator('^[A-Za-zА-Яа-яЁё0-9 _\-\'&]+$')
                             ])
 
