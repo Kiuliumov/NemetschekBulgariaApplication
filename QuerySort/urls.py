3dc483ec94478c09ca202e7d0140d07403e2bcfb
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-03 08:32:17 +0300

    DB table

--- QuerySort/urls.py
+++ QuerySort/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
+
 from QuerySort import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new-data', views.new_data, name='inject-data'),
+    path('new-data', views.new_data, name='new-data'),
 ]
