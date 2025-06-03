f2466f2c188dfa7e66b3930e28c8fa19427ed7c8
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-06-03 08:31:57 +0300

    DB table

--- QuerySort/forms.py
+++ QuerySort/forms.py
@@ -1,25 +1,25 @@
-from plistlib import Data
 from django import forms
-
-from QuerySort.models import Event, Lector
+from QuerySort.models import Event
 
 
 class EventForm(forms.ModelForm):
-    new_lectors = forms.CharField(
-        required=False,
-        help_text="Add lecturer names separated by commas"
-    )
 
     class Meta:
         model = Event
-        fields = ['name', 'town', 'date', 'type', 'lectors']
+        fields = '__all__'
 
-    def save(self, commit=True):
-        event = super().save(commit)
-        new_lectors_names = self.cleaned_data.get('new_lectors')
-        if new_lectors_names:
-            names = [name.strip() for name in new_lectors_names.split(',') if name.strip()]
-            for name in names:
-                lector, created = Lector.objects.get_or_create(name=name)
-                event.lectors.add(lector)
-        return event
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'class': 'flatpickr',
+                'placeholder': 'Избери дата',
+                'type': 'date'
+            }),
+        }
+
+        labels = {
+            'name': 'Име на събитието',
+            'town': 'Място на събитието',
+            'date': 'Дата',
+            'type': 'Тип на събитието',
+            'lectors': 'Лектори',
+        }
